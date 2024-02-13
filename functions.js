a29fd1ae927fa6bc3bb33e3f29f01319b593245e
f0rmic67 <nef9476@pennwest.edu>
2024-02-12 19:05:56 -0500

    Added functions.js for javascript functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+<script>
+    //functions to validate user input before sending to server
+    function validateSearch(){
+
+    }
+
+    //validate user input before submitting login information
+    function validateLogin(){
+
+    }
+
+    //display proper form for chosen account type
+    function accountType(){
+
+    }
+
+    //validate user input before submitting form
+    function validateStudent(){
+
+    }
+
+    //validate user input before submitting form
+    function validateEMS(){
+
+    }
+</script>
