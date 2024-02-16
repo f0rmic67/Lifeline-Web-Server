2c2236e27beb260a71a7b06b91ce39211286ba47
Anthony Stepich <anthony.stepich12@gmail.com>
2024-02-16 09:46:47 -0500

    added files so we can use jquery and sweetalert and changed structue

--- functions.js
+++ functions.js
@@ -1,15 +1,38 @@
-<script>
+function loadPage(){
+ addListeners();
+}
+function addListeners(){
+    //validate user input before submitting login information
+
+    $("#subLog").on('click touch' ,function() {
+        if ($("#pass").val() == ""  || $("#email").val() =="")
+        {
+            Swal.fire({
+                title : 'ERROR',
+                text : 'You Must fill in both fields',
+                icon : 'error',
+                confirmButtonText:'Okay'
+            })
+            $("#pass").val('');
+            $("#email").val('');
+        }
+//will have to check aginst the DB 
+
+        let password = $("#pass").val();
+        let email = $("#email").val();
+
+      
+    });
+}
+
+
     //functions to validate user input before sending to server
     function validateSearch(){
 
     }
 
-    //validate user input before submitting login information
-    function validateLogin(){
-
-    }
-
     //display proper form for chosen account type
+    //this would happen after the user has made an account and signed in 
     function accountType(){
 
     }
@@ -23,4 +46,4 @@
     function validateEMS(){
 
     }
-</script>
+
