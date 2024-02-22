f243f9c8aeda471c63c8fb4a3ed170bec734fec2
f0rmic67 <nef9476@pennwest.edu>
2024-02-22 15:43:21 -0500

    functions.js updated with input validation createAccount changed to .php file to include database connection Database connection not yet functional, still need to figure out issue, likely with connection credentials

--- createAccount.php
+++ createAccount.php
@@ -1,10 +1,46 @@
+<?php
+    //create PHP session for storing user info between pages
+    $status = session_status();
+    if($status == PHP_SESSION_NONE){
+        session_start();
+    }
+
+    //connect to database
+    $dsn = 'mysql:host=ls-3b7e6f89b80f6a9ff2d02a1394dd18e58f481247.cl444yoq0gck.us-east-2.rds.amazonaws.com;dbname=Database-1'; 
+    $username = 'dbmasteruser';
+    $password = 'Lifeline2024';
+
+    try {
+        $db = new PDO($dsn, $username, $password); //creates PDO
+        echo '<p> You are connected. </p>';
+    }
+    catch (PDOException $e){
+        $error_message = $e->getMessage();
+        echo '<p> Connection failed due to error : $error_message </p>';
+    }
+
+    $action = filter_input(INPUT_GET, 'accSubmit');
+
+    if($action == "Create Student Account"){
+        //get each user input field from html and verify data 
+        $accountType = 1; //student account type = int 1
+        $username = filter_input(INPUT_GET, 'sUser');
+        $email = filter_input(INPUT_GET, 'sEmail', FILTER_VALIDATE_EMAIL);
+        $id = filter_input(INPUT_GET, "sID", FILTER_VALIDATE_INT);
+        $password = filter_input(INPUT_GET, "sPass1");
+    }
+    else if($action == "Create EMS Account"){
+        $accountType = 2; //EMS account type = int 2
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Create Account</title>
         <link rel="stylesheet" type="text/css" href="main.css">
 
-        <script src="functions.js"></script>
+        <script type="text/javascript" src="functions.js"></script>
     </head>
     
     <body>
@@ -15,7 +51,7 @@
         <br>
         <br>
         <label>Account Type:</label>
-        <select name="accType">
+        <select id="accType" onchange="accountType()">
             <option value="student">Student</option>
             <option value="ems">EMS</option>
         </select>
@@ -23,7 +59,7 @@
 
         <!--forms will hide and display based on user input for account type-->
 
-        <form id="studentForm" onsubmit="validateStudent()" style="display:none">
+        <form id="studentForm" onsubmit="validateStudent()" action="createAccount.html" method="get" style="display:block">
             <label>Username:</label>
             <input type="text" name="sUser" value="">
             <br>
@@ -44,7 +80,7 @@
             <input type="password" name="sPass2" value="">
             <br>
             <br>
-            <input type="submit" name="studentAccSubmit" value="Create Student Account">
+            <input type="submit" name="accSubmit" value="Create Student Account">
         </form>
 
         <form id="emsForm" onsubmit="validateEMS()" style="display:none">
@@ -68,7 +104,7 @@
             <input type="password" name="sPass2" value="">
             <br>
             <br>
-            <input type="submit" name="emsAccSubmit" value="Create EMS Account">
+            <input type="submit" name="accSubmit" value="Create EMS Account">
         </form>
     </body>
 </html>
--- functions.js
+++ functions.js
@@ -1,26 +1,70 @@
-<script>
+
     //functions to validate user input before sending to server
     function validateSearch(){
-
+        
     }
 
     //validate user input before submitting login information
     function validateLogin(){
+        var loginInfo = document.getElementById("loginPage");
 
+        if(loginInfo.logEmail.value == ""){
+            alert("Email must be entered.");
+            return false;
+        }
+        else if(loginInfo.logPass.value == ""){
+            alert("Password must be entered.");
+            return false;
+        }
+
+        return true;
     }
 
     //display proper form for chosen account type
     function accountType(){
+        var accType = document.getElementById("accType");
 
+        if(accType.value == "student"){
+            document.getElementById("studentForm").style = "display:block";
+            document.getElementById("emsForm").style = "display:none";
+        }
+        else{
+            document.getElementById("studentForm").style = "display:none";
+            document.getElementById("emsForm").style = "display:block";
+        }
     }
+    
 
     //validate user input before submitting form
     function validateStudent(){
+        var studentForm = document.getElementById("studentForm");
 
+        if(studentForm.sUser.value == ""){
+            alert("Username must be entered.");
+            return false;
+        }
+        else if(studentForm.sEmail.value == ""){
+            alert("Email must be entered.");
+            return false;
+        }
+        else if(studentForm.sID.value == ""){
+            alert("Student ID must be entered.");
+            return false;
+        }
+        else if(studentForm.sPass1.value == "" || studentForm.sPass2.value == ""){
+            alert("Password must be entered.");
+            return false;
+        }
+        else if(studentForm.sPass1.value != studentForm.sPass2.value){
+            alert("Passwords entered do not match.");
+            return false;
+        }
+
+        return true;
     }
 
     //validate user input before submitting form
     function validateEMS(){
 
     }
-</script>
+
