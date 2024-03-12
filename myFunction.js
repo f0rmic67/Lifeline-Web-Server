89d145e4361031445bb59a5f620f46de72baa525
jpanaro99 <158498150+jpanaro99@users.noreply.github.com>
2024-03-12 15:38:14 -0400

    Add files via upload

--- myFunction.js
+++ myFunction.js
@@ -54,3 +54,114 @@ function login(){
 	y.style.left = "450px";
 	z.style.left = "0px";
 }
+
+type="text/javascript"
+$(function heartProblems() {
+	$("#heartProblems").change(function () {
+		if ($(this).val() == 2) {
+			$("#heartProblem").removeAttr("disabled");
+			$("#heartProblem").focus();
+		} else {
+			$("#heartProblem").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function pacemakers() {
+	$("#pacemakers").change(function () {
+		if ($(this).val() == 2) {
+			$("#pacemaker").removeAttr("disabled");
+			$("#pacemaker").focus();
+		} else {
+			$("#pacemaker").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function diabetes() {
+	$("#diabetes").change(function () {
+		if ($(this).val() == 2) {
+			$("#diabete").removeAttr("disabled");
+			$("#diabete").focus();
+		} else {
+			$("#diabete").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function highBloodPressure() {
+	$("#highBloodPressure").change(function () {
+		if ($(this).val() == 2) {
+			$("#highBP").removeAttr("disabled");
+			$("#highBP").focus();
+		} else {
+			$("#highBP").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function Strokes() {
+	$("#Strokes").change(function () {
+		if ($(this).val() == 2) {
+			$("#stroke").removeAttr("disabled");
+			$("#stroke").focus();
+		} else {
+			$("#stroke").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function asthma() {
+	$("#Asthma").change(function () {
+		if ($(this).val() == 2) {
+			$("#asthma").removeAttr("disabled");
+			$("#asthma").focus();
+		} else {
+			$("#asthma").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function Seizures() {
+	$("#Seizures").change(function () {
+		if ($(this).val() == 2) {
+			$("#seizure").removeAttr("disabled");
+			$("#seizure").focus();
+		} else {
+			$("#seizure").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function Cancer() {
+	$("#Cancer").change(function () {
+		if ($(this).val() == 2) {
+			$("#cancer").removeAttr("disabled");
+			$("#cancer").focus();
+		} else {
+			$("#cancer").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function Allergies() {
+	$("#Allergies").change(function () {
+		if ($(this).val() == 2) {
+			$("#allergy").removeAttr("disabled");
+			$("#allergy").focus();
+		} else {
+			$("#allergy").attr("disabled", "disabled");
+		}
+	});
+});
+
+$(function Other() {
+	$("#Other").change(function () {
+		if ($(this).val() == 2) {
+			$("#other").removeAttr("disabled");
+			$("#other").focus();
+		} else {
+			$("#other").attr("disabled", "disabled");
+		}
+	});
+});
